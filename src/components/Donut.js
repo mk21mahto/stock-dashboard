19a1562cd2048dd31888dea3d8109ca9fba0052c
Manish Kumar <manishmahto2196@gmail.com>
2024-03-07 16:27:01 +0530

    doughnut implementation with mockdata

--- src/components/Donut.js
+++ src/components/Donut.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Doughnut } from 'react-chartjs-2'
+import { Assets } from './mockData';
 import {
   Chart as ChartJS, ArcElement, Tooltip
 } from 'chart.js'
@@ -10,14 +11,37 @@ ChartJS.register(
 );
 
 const Donut = () => {
+
+  const [view, setView] = useState('Assets')
+  let result = []
+  const sumByType = Assets.reduce((acc, asset) => {
+    const index = acc.findIndex(item => item.type === asset.type);
+
+    if (index === -1) {
+        acc.push({ type: asset.type, sum: asset.amount });
+    } else {
+        acc[index].sum += asset.amount;
+    }
+
+    return acc;
+}, []);
+
+
+  const portfolioData = view === 'Assets' ? sumByType.map(item => item.sum) : Assets.map(asset => asset.amount)
+  const labels = view === 'Assets' ?  Assets.map(asset => {
+    if(!result.includes(asset.type)){
+      result.push(asset.type)
+    }
+    return result
+  }) : Assets.map(asset => {
+      result.push(asset.name)
+    return result
+  })
+
   const data = {
-    labels: [
-      'Stocks',
-      'Cryptocurrency',
-      'Fiat'
-    ],
+    labels:labels[0],
     datasets: [{
-      data: [300, 50, 100],
+      data: portfolioData,
       backgroundColor: [
         'rgb(255, 99, 132)',
         'rgb(54, 162, 235)',
@@ -27,8 +51,24 @@ const Donut = () => {
     }]
   }
 
+  const onOptionChange = e => {
+    setView(e.target.value)
+  }
+
   return (
-    <div><Doughnut data={data}></Doughnut></div>
+    <div className='my-5'>
+      <Doughnut data={data} />
+      <div className="flex flex-wrap">
+        <div className="flex items-center me-4">
+            <input id="assets" type="radio" value="Assets" checked={view === "Assets"} name="view" className="w-4 h-4" onChange={onOptionChange}/>
+            <label htmlFor="assets" className="ms-2 text-sm font-medium">Assets</label>
+        </div>
+        <div className="flex items-center me-4">
+            <input id="individual" type="radio" value="Individual" checked={view === "Individual"} name="view" className="w-4 h-4" onChange={onOptionChange}/>
+            <label htmlFor="individual" className="ms-2 text-sm font-medium">Individual</label>
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/mockData.js
@@ -0,0 +1,33 @@
+
+export const Assets = [
+    {
+        name: 'HDFC',
+        amount: 400,
+        type: 'Stock'
+    },
+    {
+        name: 'Google',
+        amount: 300,
+        type: 'Stock'
+    },
+    {
+        name: 'Bitcoin',
+        amount: 100,
+        type: 'Cryptocurrency'
+    },
+    {
+        name: 'Euro',
+        amount: 1100,
+        type: 'Fiat'
+    },
+    {
+        name: 'Etherum',
+        amount: 100,
+        type: 'Cryptocurrency'
+    },
+    {
+        name: 'Pound',
+        amount: 900,
+        type: 'Fiat'
+    },
+]
